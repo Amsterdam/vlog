3b9aca7388795d2410b27750ca28cd30e3d27c5b
Jeffrey Klardie <j.klardie@amsterdam.nl>
2021-03-18 13:59:26 +0100

    Black, isort, flake8 fixes in src

--- src/contrib/rest_framework/parsers.py
+++ src/contrib/rest_framework/parsers.py
@@ -1,11 +1,11 @@
 from rest_framework.parsers import BaseParser
-from contrib.rest_framework.renderers import PlainTextRenderer
 
 
 class PlainTextParser(BaseParser):
     """
     Parser for form data.
     """
+
     media_type = 'text/plain'
 
     def parse(self, stream, media_type=None, parser_context=None):
@@ -13,5 +13,4 @@ class PlainTextParser(BaseParser):
         Parses the incoming bytestream as a URL encoded form,
         and returns the resulting QueryDict.
         """
-        parser_context = parser_context or {}
         return stream.read()
