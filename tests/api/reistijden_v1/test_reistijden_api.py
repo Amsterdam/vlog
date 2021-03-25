1f229985c2238d2532e19b1bcc0f64f902f60134
Jeffrey Klardie <j.klardie@amsterdam.nl>
2021-03-25 10:29:42 +0100

    Fixed isort issues

--- tests/api/reistijden_v1/test_reistijden_api.py
+++ tests/api/reistijden_v1/test_reistijden_api.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 from ingress.models import Collection, FailedMessage, Message
-from rest_framework.test import APITestCase
-
 from reistijden_v1.consumer import ReistijdenConsumer
 from reistijden_v1.models import (
     Category,
@@ -13,6 +11,8 @@ from reistijden_v1.models import (
     Publication,
     TravelTime,
 )
+from rest_framework.test import APITestCase
+
 from tests.api.reistijden_v1.test_xml import (
     TEST_POST_EMPTY,
     TEST_POST_INDIVIDUAL_TRAVEL_TIME,
