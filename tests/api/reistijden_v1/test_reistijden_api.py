be1ba5cc073bc48ea724b8e5bffd066a255d3d87
Jeffrey Klardie <j.klardie@amsterdam.nl>
2021-04-29 14:51:54 +0200

    MeasurementLocation testcases

--- tests/api/reistijden_v1/test_reistijden_api.py
+++ tests/api/reistijden_v1/test_reistijden_api.py
@@ -2,13 +2,16 @@ from datetime import datetime
 from unittest.mock import patch
 
 from django.conf import settings
+from django.db.models import Q
 from ingress.models import Collection, FailedMessage, Message
 from reistijden_v1.consumer import ReistijdenConsumer
 from reistijden_v1.models import (
+    Camera,
     IndividualTravelTime,
     Lane,
     Measurement,
     MeasurementLocation,
+    MeasurementSite,
     Publication,
     TrafficFlow,
     TrafficFlowCategoryCount,
@@ -62,9 +65,10 @@ class ReistijdenPostTest(ReistijdenPostTestBase):
 
         ReistijdenConsumer().consume(end_at_empty_queue=True)
 
+        # assert publication existence and content
         self.assertEqual(Publication.objects.all().count(), 1)
-
         publication = Publication.objects.get()
+
         self.assertEqual(publication.type, "travelTime")
         self.assertEqual(publication.reference_id, "PUB_AMS_PRED_TRAJECTORY_TT")
         self.assertEqual(publication.version, "1.0")
@@ -83,12 +87,88 @@ class ReistijdenPostTest(ReistijdenPostTestBase):
         self.assertEqual(publication.measurement_duration, 10)
 
         self.assertEqual(Measurement.objects.all().count(), 2)
-        self.assertEqual(MeasurementLocation.objects.all().count(), 6)
-        self.assertEqual(Lane.objects.all().count(), 7)
+        self.assertEqual(MeasurementLocation.objects.all().count(), 4)
+        location_qs = MeasurementLocation.objects.filter(
+            measurement_site__reference_id="TRJ_1111"
+        )
+        self.assertEqual(location_qs.filter(index=1).count(), 1)
+        self.assertEqual(location_qs.filter(index=2).count(), 1)
+        self.assertEqual(location_qs.filter(index=3).count(), 1)
+        self.assertEqual(location_qs.filter(index=4).count(), 1)
+
+        self.assertEqual(Lane.objects.all().count(), 6)
+
+        # assert that both measurements have the same measurement site
+        self.assertEquals(
+            MeasurementSite.objects.filter(
+                reference_id="TRJ_1111",
+                name="traject_ZX1111_ZX1111",
+                version="1.0",
+                type="trajectory",
+                length=1111,
+            ).count(),
+            1,
+        )
+
+        measurement_site = MeasurementSite.objects.get()
+        self.assertEqual(Measurement.objects.count(), 2)
+        self.assertEqual(
+            Measurement.objects.filter(measurement_site=measurement_site).count(), 2
+        )
+
+        # assert traveltime existence and content
         self.assertEqual(TravelTime.objects.all().count(), 5)
+        self.assertTrue(
+            TravelTime.objects.filter(
+                type="predicted",
+                estimation_type="estimated",
+                travel_time=11,
+                traffic_speed=22,
+                data_error=False,
+                data_quality=None,
+                num_input_values_used=None,
+            ).exists()
+        )
+        self.assertTrue(
+            TravelTime.objects.filter(
+                type="processed",
+                estimation_type="estimated",
+                travel_time=178,
+                traffic_speed=34,
+                data_error=True,
+                data_quality=38.0,
+                num_input_values_used=10,
+            ).exists()
+        )
+        self.assertTrue(
+            TravelTime.objects.filter(
+                type="raw", travel_time=-1, traffic_speed=-1
+            ).exists()
+        )
+        self.assertTrue(
+            TravelTime.objects.filter(
+                type="representative", travel_time=-1, traffic_speed=-1
+            ).exists()
+        )
+
         self.assertEqual(IndividualTravelTime.objects.all().count(), 0)
         self.assertEqual(TrafficFlow.objects.all().count(), 0)
         self.assertEqual(TrafficFlowCategoryCount.objects.all().count(), 0)
+
+        self.assertEqual(Camera.objects.count(), 6)
+        self.assertTrue(
+            Camera.objects.filter(
+                reference_id="233f606b-b5f4-424e-ae2b-266e552ef444",
+                latitude=52.444444,
+                longitude=4.444444,
+                lane_number=1,
+                status="on",
+                view_direction=444,
+                lane__specific_lane=1,
+                lane__measurement_location__index=4,
+                lane__measurement_location__measurement_site__reference_id="TRJ_1111",
+            ).exists()
+        )
 
     def test_post_new_individual_travel_time(self):
         """Test posting a new vanilla individual travel time message"""
@@ -124,6 +204,22 @@ class ReistijdenPostTest(ReistijdenPostTestBase):
         self.assertEqual(Lane.objects.all().count(), 4)
         self.assertEqual(TravelTime.objects.all().count(), 0)
         self.assertEqual(IndividualTravelTime.objects.all().count(), 3)
+
+        itt = IndividualTravelTime.objects.get(
+            license_plate="ABCDEFGHIJKLMNOPQRSTUVWXYZ11111111111111"
+        )
+        self.assertEqual(itt.vehicle_category.name, "M1")
+        self.assertEqual(
+            itt.detection_start_time,
+            datetime.fromisoformat("2019-01-22T11:55:12+00:00"),
+        )
+        self.assertEqual(
+            itt.detection_end_time,
+            datetime.fromisoformat("2019-01-22T11:55:18+00:00"),
+        )
+        self.assertEqual(itt.travel_time, 1)
+        self.assertEqual(itt.traffic_speed, 111)
+
         self.assertEqual(TrafficFlow.objects.all().count(), 0)
         self.assertEqual(TrafficFlowCategoryCount.objects.all().count(), 0)
 
@@ -163,6 +259,58 @@ class ReistijdenPostTest(ReistijdenPostTestBase):
         self.assertEqual(TravelTime.objects.all().count(), 0)
         self.assertEqual(IndividualTravelTime.objects.all().count(), 0)
         self.assertEqual(TrafficFlow.objects.all().count(), 4)
+
+        traffic_flow = TrafficFlow.objects.get(
+            measurement__measurement_site__reference_id=11,
+            specific_lane="lane1",
+            vehicle_flow=6,
+        )
+        self.assertEqual(
+            TrafficFlowCategoryCount.objects.filter(traffic_flow=traffic_flow)
+            .filter(
+                Q(vehicle_category__name="Auto", count=6)
+                | Q(vehicle_category__name="Bedrijfsauto Licht", count=2)
+            )
+            .count(),
+            2,
+        )
+
+        traffic_flow = TrafficFlow.objects.get(
+            measurement__measurement_site__reference_id=22,
+            specific_lane="lane1",
+            vehicle_flow=7,
+        )
+        self.assertEqual(
+            TrafficFlowCategoryCount.objects.filter(
+                traffic_flow=traffic_flow, vehicle_category=None, count=6
+            ).count(),
+            1,
+        )
+
+        traffic_flow = TrafficFlow.objects.get(
+            measurement__measurement_site__reference_id=33,
+            specific_lane="lane1",
+            vehicle_flow=1,
+        )
+        self.assertEqual(
+            TrafficFlowCategoryCount.objects.filter(
+                traffic_flow=traffic_flow, vehicle_category__name="Auto", count=1
+            ).count(),
+            1,
+        )
+
+        traffic_flow = TrafficFlow.objects.get(
+            measurement__measurement_site__reference_id=33,
+            specific_lane="lane2",
+            vehicle_flow=2,
+        )
+        self.assertEqual(
+            TrafficFlowCategoryCount.objects.filter(
+                traffic_flow=traffic_flow, vehicle_category__name="Auto", count=2
+            ).count(),
+            1,
+        )
+
         self.assertEqual(TrafficFlowCategoryCount.objects.all().count(), 5)
 
     def test_empty_measurement(self):
@@ -172,6 +320,28 @@ class ReistijdenPostTest(ReistijdenPostTestBase):
         ReistijdenConsumer().consume(end_at_empty_queue=True)
 
         self.assertEqual(Publication.objects.all().count(), 1)
+        self.assertEqual(Measurement.objects.all().count(), 0)
+        self.assertEqual(MeasurementLocation.objects.all().count(), 0)
+        self.assertEqual(Lane.objects.all().count(), 0)
+        self.assertEqual(TravelTime.objects.all().count(), 0)
+        self.assertEqual(IndividualTravelTime.objects.all().count(), 0)
+        self.assertEqual(TrafficFlow.objects.all().count(), 0)
+        self.assertEqual(TrafficFlowCategoryCount.objects.all().count(), 0)
+
+    def test_expaterror(self):
+        response = self.client.post(self.URL, TEST_POST_WRONG_TAGS, **REQUEST_HEADERS)
+        self.assertEqual(response.status_code, 200, response.data)
+
+        ReistijdenConsumer().consume(end_at_empty_queue=True)
+
+        self.assertEqual(
+            FailedMessage.objects.filter(
+                consume_fail_info__icontains='not well-formed (invalid token): '
+                'line 11, column 5'
+            ).count(),
+            1,
+        )
+        self.assertEqual(Publication.objects.all().count(), 0)
         self.assertEqual(Measurement.objects.all().count(), 0)
         self.assertEqual(MeasurementLocation.objects.all().count(), 0)
         self.assertEqual(Lane.objects.all().count(), 0)
@@ -202,30 +372,6 @@ class ReistijdenPostTest(ReistijdenPostTestBase):
             self.URL, TEST_POST_TRAVEL_TIME, **CONTENT_TYPE_HEADER
         )
         self.assertEqual(response.status_code, 401, response.data)
-
-
-class ReistijdenPostErrorsTest(ReistijdenPostTestBase):
-    def test_expaterror(self):
-        response = self.client.post(self.URL, TEST_POST_WRONG_TAGS, **REQUEST_HEADERS)
-        self.assertEqual(response.status_code, 200, response.data)
-
-        ReistijdenConsumer().consume(end_at_empty_queue=True)
-
-        self.assertEqual(
-            FailedMessage.objects.filter(
-                consume_fail_info__icontains='not well-formed (invalid token): '
-                'line 11, column 5'
-            ).count(),
-            1,
-        )
-        self.assertEqual(Publication.objects.all().count(), 0)
-        self.assertEqual(Measurement.objects.all().count(), 0)
-        self.assertEqual(MeasurementLocation.objects.all().count(), 0)
-        self.assertEqual(Lane.objects.all().count(), 0)
-        self.assertEqual(TravelTime.objects.all().count(), 0)
-        self.assertEqual(IndividualTravelTime.objects.all().count(), 0)
-        self.assertEqual(TrafficFlow.objects.all().count(), 0)
-        self.assertEqual(TrafficFlowCategoryCount.objects.all().count(), 0)
 
     def test_post_wrongly_formatted_xml(self):
         response = self.client.post(
