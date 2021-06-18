3f49e6a317d88ceaa622032e4e40c6f1f6810cbf
Jeffrey Klardie <j.klardie@amsterdam.nl>
2021-06-18 10:48:57 +0200

    After data migration assert data sanity (02)

--- src/reistijden_v1/management/commands/01_migrate_individual_travel_time_vehicle_categories.py
+++ src/reistijden_v1/management/commands/01_migrate_individual_travel_time_vehicle_categories.py
@@ -1,13 +1,15 @@
 import logging
 
 from django.core.management import BaseCommand
+from django.db.models import F
 
+from reistijden_v1.management.commands.base_command import MyCommand
 from reistijden_v1.models import IndividualTravelTime, VehicleCategory
 
 logger = logging.getLogger(__name__)
 
 
-class Command(BaseCommand):
+class Command(MyCommand):
     def add_arguments(self, parser):
         parser.add_argument('--sleep', nargs='?', default=1, type=int)
 
@@ -23,6 +25,9 @@ class Command(BaseCommand):
             .distinct()
         )
 
+        num_categories = unique_categories.count()
+        self.success(f"Found {num_categories} unique categories. Migrating...")
+
         category_dict = {}
 
         for category in unique_categories:
@@ -30,8 +35,29 @@ class Command(BaseCommand):
             category_dict[category] = vehicle_category.id
 
         for category, vehicle_category_id in category_dict.items():
+            self.notice(f"- Migrating {category}...")
             IndividualTravelTime.objects.filter(
                 vehicle_category_id=None, old_vehicle_category=category
             ).update(vehicle_category_id=vehicle_category_id)
 
-        self.stdout.write(self.style.SUCCESS('Finished'))
+        num_categories = VehicleCategory.objects.count()
+        self.success(f"FINISHED! Migrated {num_categories} categories")
+
+        self.validate_results(num_objects=num_categories)
+
+    def validate_results(self, num_objects):
+        self.notice("Validating results")
+        num_errors = IndividualTravelTime.objects.exclude(
+            old_vehicle_category=F('vehicle_category__name')
+        ).count()
+
+        if num_errors > 0:
+            self.error(
+                f"ERROR! Found {num_errors} IndividualTravelTime objects "
+                f"(out of {num_objects}) where category is wrong."
+            )
+        else:
+            self.success(
+                f"SUCCESS! All IndividualTravelTime objects ({num_objects}) "
+                f"have a correct category."
+            )
