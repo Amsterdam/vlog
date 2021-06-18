8036e4edb32fb4c098e80d1140d867f493902b6d
Jeffrey Klardie <j.klardie@amsterdam.nl>
2021-06-18 09:44:15 +0200

    Added print_data_stats command

--- src/reistijden_v1/management/commands/print_data_stats.py
+++ src/reistijden_v1/management/commands/print_data_stats.py
@@ -1,17 +1,14 @@
 from django.core.management.base import BaseCommand
 
 from reistijden_v1.models import (
-    Camera,
-    IndividualTravelTime,
-    Lane,
-    Measurement,
-    MeasurementLocation,
-    MeasurementSite,
     Publication,
-    TrafficFlow,
-    TrafficFlowCategoryCount,
+    Measurement,
+    Location,
+    Lane,
     TravelTime,
-    VehicleCategory,
+    IndividualTravelTime,
+    MeasuredFlow,
+    Category,
 )
 
 
@@ -22,15 +19,12 @@ class Command(BaseCommand):
         for model in [
             Publication,
             Measurement,
-            MeasurementSite,
-            MeasurementLocation,
+            Location,
             Lane,
-            Camera,
             TravelTime,
             IndividualTravelTime,
-            TrafficFlow,
-            TrafficFlowCategoryCount,
-            VehicleCategory,
+            MeasuredFlow,
+            Category,
         ]:
             num_objects = model.objects.count()
             self.stdout.write(f"Num {model.__name__} objects: {num_objects}")
