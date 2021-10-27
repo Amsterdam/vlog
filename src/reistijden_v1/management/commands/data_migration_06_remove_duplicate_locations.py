3e6222e3a502a7fd449cdf644d2f23770d279683
Daniel Bradburn <moagstar@gmail.com>
2021-10-27 10:52:51 +0200

    Migration 06 : Remove duplicate locations that do not refer to a lane

---
+++ src/reistijden_v1/management/commands/data_migration_06_remove_duplicate_locations.py
@@ -0,0 +1,15 @@
+import logging
+
+from src.reistijden_v1.management.commands.data_migration_command import (
+    DataMigrationCommand,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class Command(DataMigrationCommand):
+
+    DELETE_QUERY = """
+        DELETE FROM reistijden_v1_measurementlocation
+        WHERE id NOT in (SELECT measurement_location_id FROM reistijden_v1_lane)
+    """
