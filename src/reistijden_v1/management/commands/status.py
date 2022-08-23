3c196a4cb365d52885b08c4dea2e47730628b8fa
Daniel Bradburn <moagstar@gmail.com>
2022-08-23 09:44:44 +0200

    add extra info to status command

--- src/reistijden_v1/management/commands/status.py
+++ src/reistijden_v1/management/commands/status.py
@@ -5,19 +5,36 @@ from django.core.management import BaseCommand
 from django.db import connection
 from ingress.models import FailedMessage, Message
 
-from reistijden_v1.models import Measurement, Measurement2, MeasurementSite, Publication
+from reistijden_v1.models import (
+    Camera,
+    Lane2,
+    Measurement,
+    Measurement2,
+    MeasurementLocation2,
+    MeasurementSite,
+    Publication,
+)
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--with-measurement-counts', action='store_true')
+
     def handle(self, *args, **options):
 
         publication_timestamp = None
         if publication := Publication.objects.last():
             publication_timestamp = publication.measurement_start_time
 
-        last_measurement2 = Measurement2.objects.latest('id')
+        if options['with_measurement_counts']:
+            measurement_counts = {
+                'measurement_count': Measurement.objects.count(),
+                'measurement2_count': Measurement2.objects.count(),
+            }
+        else:
+            measurement_counts = {}
 
         print(
             json.dumps(
@@ -25,6 +42,9 @@ class Command(BaseCommand):
                     'message_count': Message.objects.count(),
                     'failed_message_count': FailedMessage.objects.count(),
                     'measurement_site_count': MeasurementSite.objects.count(),
+                    'measurement_location_count': MeasurementLocation2.objects.count(),
+                    'lane_count': Lane2.objects.count(),
+                    'camera_count': Camera.objects.count(),
                     'last_publication_timestamp': publication_timestamp,
                     'measurements': (
                         Measurement.objects.earliest('id').id,
@@ -32,21 +52,9 @@ class Command(BaseCommand):
                     ),
                     'measurement2': (
                         Measurement2.objects.earliest('id').id,
-                        last_measurement2.id,
+                        Measurement2.objects.latest('id').id,
                     ),
-                    'measurement2_next_batch': (
-                        Measurement.objects.filter(
-                            id__range=(
-                                last_measurement2.id + 1,
-                                last_measurement2.id + 10_001,
-                            )
-                        ).count()
-                    ),
-                    'first_measurement_after_last_measurement2': (
-                        Measurement.objects.filter(id__gt=last_measurement2.id)
-                        .earliest('id')
-                        .id
-                    ),
+                    **measurement_counts,
                 },
                 indent=2,
                 default=str,
