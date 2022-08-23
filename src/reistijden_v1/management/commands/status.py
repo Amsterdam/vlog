994779665ec617246e0709da7cc2599895990ac4
Daniel Bradburn <moagstar@gmail.com>
2022-08-23 10:25:26 +0200

    add extra info to status command

--- src/reistijden_v1/management/commands/status.py
+++ src/reistijden_v1/management/commands/status.py
@@ -5,17 +5,36 @@ from django.core.management import BaseCommand
 from django.db import connection
 from ingress.models import FailedMessage, Message
 
-from reistijden_v1.models import MeasurementSite, Publication
+from reistijden_v1.models import (
+    Camera,
+    Lane2,
+    Measurement,
+    Measurement2,
+    MeasurementLocation2,
+    MeasurementSite,
+    Publication,
+)
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--with-measurement-counts', action='store_true')
+
     def handle(self, *args, **options):
 
         publication_timestamp = None
         if publication := Publication.objects.last():
             publication_timestamp = publication.measurement_start_time
+
+        if options['with_measurement_counts']:
+            measurement_counts = {
+                'measurement_count': Measurement.objects.count(),
+                'measurement2_count': Measurement2.objects.count(),
+            }
+        else:
+            measurement_counts = {}
 
         print(
             json.dumps(
@@ -23,8 +42,21 @@ class Command(BaseCommand):
                     'message_count': Message.objects.count(),
                     'failed_message_count': FailedMessage.objects.count(),
                     'measurement_site_count': MeasurementSite.objects.count(),
+                    'measurement_location_count': MeasurementLocation2.objects.count(),
+                    'lane_count': Lane2.objects.count(),
+                    'camera_count': Camera.objects.count(),
                     'last_publication_timestamp': publication_timestamp,
+                    'measurements': (
+                        Measurement.objects.earliest('id').id,
+                        Measurement.objects.latest('id').id,
+                    ),
+                    'measurement2': (
+                        Measurement2.objects.earliest('id').id,
+                        Measurement2.objects.latest('id').id,
+                    ),
+                    **measurement_counts,
                 },
                 indent=2,
+                default=str,
             )
         )
