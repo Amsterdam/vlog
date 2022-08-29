76118ad9121cd4164980fd24854ae6415b3326be
Daniel Bradburn <moagstar@gmail.com>
2022-08-29 11:23:22 +0200

    no need to remove fields from tables which are about to be dropped

--- src/reistijden_v1/migrations/0019_cleanup_old_models.py
+++ src/reistijden_v1/migrations/0019_cleanup_old_models.py
@@ -10,14 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='measurementlocationold',
-            name='measurement',
-        ),
-        migrations.RemoveField(
-            model_name='measurementold',
-            name='publication',
-        ),
         migrations.DeleteModel(
             name='LaneOld',
         ),
